0d3e4e82e8322bf6fa401c0ad271a886e743d9d6
Alex Flinchum <alexanderflinchum@gmail.com>
2026-06-16 17:03:10 -0700

    implemented middleware for authorization of graphql requests as well as added user context

---
+++ auth/middleware.go
@@ -0,0 +1,55 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	jwtlib "fitquest-backend/jwt"
+)
+
+type contextKey struct {
+	name string
+}
+
+var userCtxKey = &contextKey{"user"}
+
+type UserCtx struct {
+	UserID   string
+	Username string
+}
+
+func Middleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			header := r.Header.Get("Authorization")
+
+			if header == "" {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			tokenStr := strings.TrimPrefix(header, "Bearer ")
+
+			claims, err := jwtlib.ParseToken(tokenStr)
+			if err != nil {
+				http.Error(w, "Invalid token", http.StatusForbidden)
+				return
+			}
+
+			user := &UserCtx{
+				UserID:   claims.UserID,
+				Username: claims.Username,
+			}
+
+			ctx := context.WithValue(r.Context(), userCtxKey, user)
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func ForContext(ctx context.Context) *UserCtx {
+	raw, _ := ctx.Value(userCtxKey).(*UserCtx)
+	return raw
+}
